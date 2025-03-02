4a53bea3f3d27d482652930286c32b3fb56a1ba6
Nithin George <nithingeorge3@gmail.com>
2025-03-02 00:34:59 +0000

    Project added

---
+++ Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Nitin George on 02/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
