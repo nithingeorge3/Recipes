dc0de6fcdf408d00da539889c89b4dbf3bf74f52
Nithin George <nithingeorge3@gmail.com>
2025-03-04 09:40:24 +0000

    code refactor

--- Recipes/Modules/RecipeDomain/Sources/RecipeDomain/Common/Country/Country.swift
+++ Recipes/Modules/RecipeDomain/Sources/RecipeDomain/Common/Country/Country.swift
@@ -13,10 +13,4 @@ public enum Country: String, Codable {
     case gb = "GB"
     case pl = "PL" //we can add more here
     case unknown
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        let rawValue = try container.decode(String.self)
-        self = Country(rawValue: rawValue) ?? .unknown
-    }
 }
