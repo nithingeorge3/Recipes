d8af3deb55f036e6d36a1c4aa6701e806363ef80
Nithin George <nithingeorge3@gmail.com>
2025-05-19 11:33:13 +0100

    adding xcode modules

---
+++ RecipeData/RecipeData/RecipeData.h
@@ -0,0 +1,18 @@
+//
+//  RecipeData.h
+//  RecipeData
+//
+//  Created by Nitin George on 19/05/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecipeData.
+FOUNDATION_EXPORT double RecipeDataVersionNumber;
+
+//! Project version string for RecipeData.
+FOUNDATION_EXPORT const unsigned char RecipeDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecipeData/PublicHeader.h>
+
+
---
+++ RecipeNetworking/RecipeNetworking/RecipeNetworking.h
@@ -0,0 +1,18 @@
+//
+//  RecipeNetworking.h
+//  RecipeNetworking
+//
+//  Created by Nitin George on 19/05/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RecipeNetworking.
+FOUNDATION_EXPORT double RecipeNetworkingVersionNumber;
+
+//! Project version string for RecipeNetworking.
+FOUNDATION_EXPORT const unsigned char RecipeNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RecipeNetworking/PublicHeader.h>
+
+
