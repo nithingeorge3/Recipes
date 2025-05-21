a417faf9f29afca8476b6d9e5917961c363ff701
Nithin George <nithingeorge3@gmail.com>
2025-05-21 18:43:27 +0100

    WIP

--- RecipeFlow/RecipeSnapshotTests/RecipeListViewSnapTests.swift
+++ RecipeFlow/RecipeSnapshotTests/RecipeListViewSnapTests.swift
@@ -42,4 +42,67 @@ final class RecipeListViewSnapTests: XCTestCase {
             named: "DefaultRecipeDetail"
         )
     }
+    
+    func testNoImageView() {
+        let viewModel = PreviewDetailViewModel.noImageRecipe
+        
+        let view = RecipeDetailView(viewModel: viewModel)
+                .frame(width: 375, height: 812)
+                .environmentObject(TabBarVisibility())
+        
+        assertSnapshot(
+            matching: view.toVC(),
+            as: .image(on: .iPhone13Mini(.portrait)),
+            named: "NoRecipeImage"
+        )
+    }
+    
+    func testNoDescriptionView() {
+        let viewModel = PreviewDetailViewModel.noDescriptionRecipe
+        
+        let view = RecipeDetailView(viewModel: viewModel)
+                    .frame(width: 375, height: 812)
+                    .environmentObject(TabBarVisibility())
+        
+        assertSnapshot(
+            matching: view.toVC(),
+            as: .image(on: .iPhone13Mini(.portrait)),
+            named: "NoRecipeDescription"
+        )
+    }
+    
+    func testFavoriteStateView() {
+        let viewModel = PreviewDetailViewModel.fullRecipe
+        
+        let view = RecipeDetailView(viewModel: viewModel)
+                    .frame(width: 375, height: 812)
+                    .environmentObject(TabBarVisibility())
+        
+        assertSnapshot(
+            matching: view.toVC(),
+            as: .image(on: .iPhone13Mini(.portrait)),
+            named: "DefaultDetail"
+        )
+    }
+    
+    private func assertSnapshot(
+        matching value: UIViewController,
+        as snapshotting: Snapshotting<UIViewController, UIImage>,
+        named name: String? = nil,
+        file: StaticString = #file,
+        testName: String = #function,
+        line: UInt = #line
+    ) {
+        let failure = verifySnapshot(
+            of: value,
+            as: snapshotting,
+            named: name,
+            file: file,
+            testName: testName,
+            line: line
+        )
+        
+        guard let message = failure else { return }
+        XCTFail(message, file: file, line: line)
+    }
 }
